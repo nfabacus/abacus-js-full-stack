447493bbed7dd8f77a4cdc911ce73fa3ecf2a7c2
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-05-16 06:42:44 +0100

    working on overflow on small screensize.

--- front-end/src/components/landingPage/bannerSection.js
+++ front-end/src/components/landingPage/bannerSection.js
@@ -4,13 +4,16 @@ const BannerSection = ()=>
   <section id="banner">
     <div className="container">
       <div className="row">
-        <div className="col-md-12 text-center">
-          <h1>Abacus Learning Lab</h1>
-          <h2>Place to learn & practice abacus, maths and more!</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-          <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <div className="col-md-12 text-center card p-5">
+          <h1 className="display-2">Abacus Learning Lab</h1>
+          <h2 className="display-4">Learn & practice abacus, maths and more!</h2>
         </div>
       </div>
+      <div className="row p-5 strapline">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+      </div>
+
       <div className="row">
         <div className="col-md-12 text-center">
           <a href=""><i className="fa fa-chevron-down fa-3x" aria-hidden="true"></i></a>
--- front-end/src/components/landingPage/socialSection.js
+++ front-end/src/components/landingPage/socialSection.js
@@ -6,17 +6,17 @@ const SocialSection = ()=>
       <div className="row">
         <div className="col-md-6">
           <h2>Connect with us:</h2>
-          <hr/>
+          <hr className="mx-0" width="250px"/>
           <p>Etiam aliquet erat non justo varius pellentesque. Quisque viverra nec lacus volutpat ornare. Etiam molestie nisl odio. </p>
         </div>
         <div className="col-md-6">
           <ul className="list-inline">
-            <li className="list-inline-item"><a href="#"><i className="fa fa-youtube"></i></a></li>
-            <li className="list-inline-item"><a href="#"><i className="fa fa-facebook"></i></a></li>
-            <li className="list-inline-item"><a href="#"><i className="fa fa-google-plus"></i></a></li>
-            <li className="list-inline-item"><a href="#"><i className="fa fa-fa-twitter"></i></a></li>
-            <li className="list-inline-item"><a href="#"><i className="fa fa-linkedin"></i></a></li>
-            <li className="list-inline-item"><a href="mailto:contact@abacusmaths.info" target="_blank"><i className="fa fa-envelope-o"></i></a></li>
+            <li className="list-inline-item"><a href="#"><i className="fa fa-youtube fa-2x"></i></a></li>
+            <li className="list-inline-item"><a href="#"><i className="fa fa-facebook fa-2x"></i></a></li>
+            <li className="list-inline-item"><a href="#"><i className="fa fa-google-plus fa-2x"></i></a></li>
+            <li className="list-inline-item"><a href="#"><i className="fa fa-twitter fa-2x"></i></a></li>
+            <li className="list-inline-item"><a href="#"><i className="fa fa-linkedin fa-2x"></i></a></li>
+            <li className="list-inline-item"><a href="mailto:contact@abacusmaths.info" target="_blank"><i className="fa fa-envelope-o fa-2x"></i></a></li>
           </ul>
         </div>
       </div>
